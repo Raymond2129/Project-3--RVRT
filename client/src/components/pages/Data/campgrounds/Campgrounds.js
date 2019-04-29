1799d0fc381ad8667b56a6e72edd305b67c89958
Kaloli <rvellark@gmail.com>
2019-04-29 16:04:29 -0600

    update with images on each card

--- client/src/components/pages/Data/campgrounds/Campgrounds.js
+++ client/src/components/pages/Data/campgrounds/Campgrounds.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
 // import Button from 'react-toolbox/lib/button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import camp from './images/camp.jpg';
 
 
 const dummyText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
 
 const Campgrounds = () => (
-  <Card style={{width: '350px'}}>
+  <Card style={{width: '250px'}}>
     <CardTitle
-      avatar="https://placeimg.com/80/80/animals"
+      avatar={camp}
       title="Avatar style title"
       subtitle="Subtitle here"
     />

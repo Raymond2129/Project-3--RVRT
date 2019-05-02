1c831cba84b269d4e43e61491069217c3973bdee
Melissa <honeybee3223@gmail.com>
2019-05-02 16:51:24 -0600

    Heroku

---
+++ client/build/precache-manifest.429c019f90ac85428ad5cfb694037c42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2a49791f27870c55f4c1de4f223e9cd",
+    "url": "/static/media/van.f2a49791.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "907bc6f5293ad2507718",
+    "url": "/static/js/main.c00c926d.chunk.js"
+  },
+  {
+    "revision": "c6597030a9206dfe47dc",
+    "url": "/static/js/2.7c059f38.chunk.js"
+  },
+  {
+    "revision": "907bc6f5293ad2507718",
+    "url": "/static/css/main.18a28289.chunk.css"
+  },
+  {
+    "revision": "c6597030a9206dfe47dc",
+    "url": "/static/css/2.3806ed09.chunk.css"
+  },
+  {
+    "revision": "adaca277659a711f7eea5cf8ccd4888c",
+    "url": "/index.html"
+  }
+];
